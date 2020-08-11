c52d9eccbb92300111554413af347c07adbfc933
EventlessKarma <max-porter@outlook.com>
2020-08-11 14:09:34 +0100

    website

---
+++ website/send_graphs.py
@@ -0,0 +1,77 @@
+import requests
+
+
+def send_graphs(user_id: int) -> str:
+
+    to_send = """<!DOCTYPE HTML>
+<html>
+<head>  
+<script>
+window.onload = function () {{
+	
+var chart1 = new CanvasJS.Chart("chartContainer", {{
+	animationEnabled: true,
+	
+	title:{{
+		text:"todays d20 rolls"
+	}},
+	axisX:{{
+		interval: 1
+	}},
+	axisY2:{{
+		interlacedColor: "rgba(1,77,101,.2)",
+		gridColor: "rgba(1,77,101,.1)",
+		title: "roll frequency"
+	}},
+	data: [{{
+		type: "bar",
+		name: "rolls",
+		axisYType: "secondary",
+		color: "#014D65",
+		{}
+		
+		
+		
+		]
+	}}]
+}});
+chart1.render();
+}}
+
+</script>
+</head>
+
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
+""".format(data_points(get_rolls(user_id, 20)))
+
+    return to_send
+
+def get_rolls(user_id, dice=20):
+
+	key = {
+		'id': user_id,
+		'dice': dice,
+		'date': 'today'
+	}
+
+	r = requests.get('http://localhost:9997', params=key)
+	
+	return r.text.split(' ')
+
+
+def data_points(db_rolls: list) -> str:
+
+    db_rolls = db_rolls[1:-1]
+    to_return = "dataPoints: [ "
+
+    for i in range(len(db_rolls)):
+        to_return += "{{ y: {}, label: '{}' }},".format(db_rolls[i], i + 1)
+
+    to_return = to_return[:-1]
+
+    return to_return
+
